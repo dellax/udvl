2605c4b817d1b01897e00a0db0bfbc9a18f35304
dellax <michalnebbia@gmail.com>
2015-05-07 23:29:35 +0200

    cviko 9

--- cv09/satsolver.py
+++ cv09/satsolver.py
@@ -41,6 +41,7 @@ class Var:
 
     def __repr__(self):
         return str(self)
+
     def __str__(self):
         return 'V(%d:%s)' % (self.n, 'u' if not self.isSet else ( 'T' if self.val else 'F' ) )
 
@@ -64,6 +65,7 @@ class Lit:
 
     def isSet(self):
         return self.var.isSet
+
     def isTrue(self):
         return self.sgn == self.var.val
 
@@ -86,6 +88,7 @@ class Clause(list):
         self.watched = [ None, None ]
 
     def __hash__(self):
+
         return id(self)
     def __str__(self):
         return 'C(%d,%s)' % (self.n,repr(self))
@@ -107,9 +110,21 @@ class Clause(list):
         Returns True if it found a new lit (or if old is ok),
         False if there is no other acceptable literal (old won't be changed).
         """
-        # TODO
-        pass
+        # zrobene na cviku
+        if self.watched[0] == old:
+            wi = 0
+        elif self.watched[1] == old:
+            wi = 1
+        else:
+            raise Exception("Literal %r not watched in %r" %(old, self))
 
+        for l in self: #prejdeme vsetky literaly v nasej clause
+            if l == self.watched[ (wi+1) % 2 ]: #preskocime druhy watch
+                continue
+            if not l.isSet() or l.isTrue(): #je nenastaveny alebo tru
+                self.setWatch(wi, l) #dame ho ako watched
+                return True
+        return False # nenasli sme iny watched
 
 class Solver:
     class InputError(Exception):
@@ -121,6 +136,7 @@ class Solver:
         self.maxVar = 0
         self.nVars = 0
         self.nCls = 0
+        self.assignedLiterals = []
 
     def read(self, ifName):
         """ Reads cnf from input file ifName.
@@ -193,7 +209,7 @@ class Solver:
             of = open(ofName, "w")
         with of:
             if sat:
-                log('SAT')
+                #log('SAT')
                 of.write('SAT\n')
                 for n in range(1,self.maxVar+1):
                     isTrue = False
@@ -202,18 +218,94 @@ class Solver:
                     of.write( ('%d ' if isTrue else '-%s ') % n )
                 of.write('0\n')
             else:
-                log('UNSAT')
+                #log('UNSAT')
                 of.write('UNSAT\n')
 
 
 
     def solve(self, ifName, ofName):
         self.read(ifName)
-        # TODO
+        # urobene na cviku
+
+        sat = self.init()
+        if sat: #este sme ok, tka backtrackujme
+            sat = self.dpll()
         sat = False
         self.write(ofName, sat)
 
+    def dpll(self):
+        #dokoncit ToDo
+        if(len(self.assignedLiterals)) == self.nVars:
+            #vsetko ohod., nasli sme riesenie
+            return True
+        l = self.chooseBranchingLiteral()
+        for l in (l, l.neg):#skisime nastavit l alebo jeho neg
+            #while(len(self.assigned....):
+             #   self.unsetLiteral()
+            pass
 
+    def init(self):
+        """Vrati false ak uz je nepslnitelne"""
+        self.assignedLiterals = []
+        self.unitClauses = []
+        #zbavime sa opakovanych literalov
+        self.clauses = [ list(set(x)) for x in self.clauses ]
+        for c in self.clauses:
+            if len(c) == 0:
+                #log("prazdna klauza")
+                return False
+            elif len(c) == 1:
+                c.setWatch(0, c[0]) #ukazuju obuidva na ten isty
+                c.setWatch(1, c[0])
+                self.unitClauses.append(  (c, c[0]) ) #jednotkova klauza
+            else:
+                c.setWatch(0, c[0])
+                c.setWatch(1, c[1])
+        return self.unitPropagate() #rovno odpropagujme unit klauzy
+
+    def setLiteral(self, l):
+        """Vrati False ak uz je nesplnitelne po nastaveni tohto lit"""
+        #log("Nastavujem $s na True" % l)
+        l.setTrue()
+        l.assignedLiterals.append(l)
+
+        #vezmeme opacny literal, ktory sme nastavili na False
+        nl = l.neg
+        #prejdeme klauzy, kde je nl watched
+        for c in nl.watchedIn[:]:#iterujeme cez kopiu (lebo z nej mazeme)
+            if c.findNewWatch(nl) == False: #nepodarilo sa najst novy
+                if nl == c.watched[0]:
+                    otherL = c.watched[1]
+                else:
+                    otherL = c.watched[0]
+                if otherL.isSet() and not otherL.isTrue(): #ak druhje je false
+                    return False #obidva watched su uz alse => UNSAT
+                elif not otherL.isSet():
+                    #druhy je posledny nenstaveny => unit clause
+                    self.unitClauses.append( (c, otherL) )
+                #else otherL je true, co nam nevadi....
+        return True
+
+    def unsetLiteral(self):
+        """Odnastavi posledne nasteveny litera"""
+        l = self.assignedLiterals.pop()
+        #log("Odnastavujem $s" % l)
+        l.unset()
+
+    def unitPropagate(self):
+        """Vrati false, ak uz je nesplnitelna"""
+        while self.unitClauses: #kym mam nejaku jednotkovu klauzu
+            c, l = self.unitClauses.pop() #vyberiem jednu
+            #log("Propagujem jednotkovu klauzu $s (lit $s)", c, l)
+            if l.isSet(): # uz je nastaveny
+                if not l.isTrue():#ale opacne
+                    self.unitClauses = []
+                    return False #konflikt => UNSAT
+            else: #este nenastaveny
+                self.unitClauses = []
+                if self.setLiteral(l) == False: #vyrobili sme UNSAT
+                    return False
+        return True
 
 if __name__ == "__main__":
     import sys
